9fd0e14562b01c999e811aa4b68701bd18144e25
Aleksey Cheusov <vle@gmx.net>
2004-04-22 18:39:19 +0000

    Useful non-GUI get-XXX functions have been moved to dictem-prog.el file Low-level functions have been moved to dictem-ll.el file

--- dictem-ll.el
+++ dictem-ll.el
@@ -24,4 +24,44 @@
       (setq str (replace-match "" t t str)))
   str)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;      Low Level Functions     ;;;;;
+
+(defun dictem-select (prompt alist default history)
+  (let
+      ((completion-ignore-case t))
+    (completing-read
+     (concat prompt " (" default "): ")
+     alist
+     nil
+     t
+     nil
+     history
+     default)))
+
+(defun dictem-get-first-token ()
+  (let
+      ((str (thing-at-point 'line)))
+    (if (string-match "^ [^ ][^ ]*" str )
+	(list (substring str (+ (match-beginning 0) 1) (match-end 0))))))
+
+(defun dictem-get-first-tokens-from-temp-buffer ()
+;    (switch-to-buffer dictem-temp-buffer-name)
+  (save-excursion
+    (set-buffer dictem-temp-buffer-name)
+    (beginning-of-buffer)
+    (let ((list-of-first-tokens nil )) ;(dictem-get-first-tokens)))
+      (while (= (forward-line 1) 0)
+	(setq
+	 list-of-first-tokens
+	 (append (dictem-get-first-token) list-of-first-tokens)))
+      list-of-first-tokens)))
+
+(defun dictem-tokenize (s)
+  (if (string-match "\"[^\"]+\"\\|[^ \"]+" s )
+;	(substring s (match-beginning 0) (match-end 0))
+      (cons (substring s (match-beginning 0) (match-end 0)) 
+	    (dictem-tokenize (substring s (match-end 0))))
+    nil))
+
 (provide 'dictem-ll)
---
+++ dictem-vars.el
@@ -0,0 +1,107 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;         Custom Things        ;;;;;
+
+(defgroup dictem nil
+  "Client for accessing the DICT server."
+  :group 'help
+  :group 'hypermedia
+  )
+
+(defcustom dictem-server "dict.org"
+  "The DICT server"
+  :group 'dictem
+  :type 'string
+  )
+
+(defcustom dictem-port "2628"
+  "The port of the DICT server"
+  :group 'dictem
+  :type 'string
+  )
+
+(defcustom dictem-client-prog "dict"
+  "The command line DICT client.
+dictem accesses DICT server through this executable.
+dict-1.9.14 or later (or compatible) is recomented."
+  :group 'dictem
+  :type 'string
+  )
+
+(defcustom dictem-default-strategy "."
+  "The default search strategy."
+  :group 'dictem
+  :group 'string
+  )
+
+(defcustom dictem-default-database "*"
+  "The default database name."
+  :group 'dictem
+  :group 'string
+  )
+
+(defcustom dictem-mode-hook
+  nil
+  "Hook run in dictem mode buffers.")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;           Variables          ;;;;;
+
+(defvar dictem-strategy-list
+  '(("word"    nil)
+    ("exact"     nil)
+    ("prefix"    nil)
+    ("substring" nil)
+    ("suffix"  nil)
+    ("re"      nil)
+    ("regexp"  nil)
+    ("soundex" nil)
+    ("lev"     nil)
+    )
+
+  "ALIST of search strategies")
+
+(defvar dictem-database-list
+  '(("elements" nil )
+    ("web1913" nil )
+    ("wn" nil )
+    ("gazetteer" nil )
+    ("jargon" nil )
+    ("foldoc" nil )
+    ("easton" nil )
+    ("hitchcock" nil )
+    ("devils" nil )
+    ("world02" nil )
+    ("vera" nil )
+    )
+
+  "ALIST of databases")
+
+(defvar dictem-strategy-history
+  nil
+  "List of strategies entered from minibuffer")
+
+(defvar dictem-database-history
+  nil
+  "List of database names entered from minibuffer")
+
+(defvar dictem-query-history
+  nil
+  "List of queries entered from minibuffer")
+
+(defvar dictem-last-database
+  "*"
+  "Last used database name")
+
+(defvar dictem-last-strategy
+  "."
+  "Last used strategy name")
+
+(defvar dictem-mode-map
+  nil
+  "Keymap for dictem mode")
+
+(defvar dictem-temp-buffer-name
+  "*dict-temp*"
+  "Temporary buffer name")
+
+(provide 'dictem-vars)
