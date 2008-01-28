f8b5e68d029a787148639f70d7bca6395e5663cd
Aleksey Cheusov <vle@gmx.net>
2008-01-28 21:04:48 +0000

    version bumped to 0.82 version 0.82 release notes

--- dictem.el
+++ dictem.el
@@ -192,7 +192,7 @@ a single word in a MATCH search."
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;           Variables          ;;;;;
 
-(defconst dictem-version "0.81"
+(defconst dictem-version "0.82"
   "DictEm version information.")
 
 (defvar dictem-strategy-alist
