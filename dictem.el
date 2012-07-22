8194c75d433557f54ab7ef61ec41819fb3cb9000
Aleksey Cheusov <vle@gmx.net>
2012-07-22 13:21:54 +0300

    version -> 1.0.3; update NEWS

--- dictem.el
+++ dictem.el
@@ -192,7 +192,7 @@ a single word in a MATCH search."
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;           Variables          ;;;;;
 
-(defconst dictem-version "1.0.2"
+(defconst dictem-version "1.0.3"
   "DictEm version information.")
 
 (defvar dictem-strategy-alist
