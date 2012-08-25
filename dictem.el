2381385898693aaac2fa681e31d74fd06a37bc42
Aleksey Cheusov <vle@gmx.net>
2012-08-25 23:26:15 +0300

    Update to 1.0.4   - Flag (interactive) was removed from the following functions:      dictem-select-strategy, dictem-select-database, dictem-read-query      and dictem-run. This fixes Debian bug #564251.

--- dictem.el
+++ dictem.el
@@ -192,7 +192,7 @@ a single word in a MATCH search."
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;           Variables          ;;;;;
 
-(defconst dictem-version "1.0.3"
+(defconst dictem-version "1.0.4"
   "DictEm version information.")
 
 (defvar dictem-strategy-alist
